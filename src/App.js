82c809222915eef88e80aeff7f0b3007abf5812f
Zenny-Aledeh <113387824+Zenny-Aledeh@users.noreply.github.com>
2023-05-15 08:58:34 +0100

    created form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./ZA Logo (1).png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Zeinab Abu</footer>
+      </div>
     </div>
   );
 }
